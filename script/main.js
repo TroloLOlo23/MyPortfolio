e2c4482b51ec2c510fb5b3d7f2c83cfe6cd015e7
TroloLOlo23 <peetrovicnemanja@gmail.com>
2024-04-07 23:54:30 +0200

    hero section finished

---
+++ script/main.js
@@ -0,0 +1,7 @@
+// const menuBar = document.querySelector('#menu-bar');
+
+// menuBar.addEventListener('click', ()=>{
+//     let navBar = document.querySelector('.nav-bar');
+//     navBar.classList.toggle("active");
+// });
+
